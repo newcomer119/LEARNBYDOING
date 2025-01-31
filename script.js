d685ac80ff3a0b29461be86a316f59ac9ea62659
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-01-31 11:11:03 +0530

    made changes

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ function revealMessage() {
         document.getElementById('message').textContent = "You're getting warmer!";
     } else if (clicks === 5) {
         document.getElementById('message').textContent = "Check the console (F12)";
-        console.log("Looking for something? Try base64: Q1NDVEz7dzNsY29tM190MF90aDNfajB1cm4zeX0=");
+        console.log("Looking for something? Try base64: ezN3bGNvbTNfdDBfdGgzX2owdXJuM3l9");
     }
 }
